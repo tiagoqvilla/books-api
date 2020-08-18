442fa2f60f4329fa6e6fa765e7feef7d78f630cc
Tiago Villa <tiago_villa@hotmail.com>
2020-08-18 19:18:52 -0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  PORT: process.env.PORT || 3000,
-  DB_CONNECTION:
-    process.env.DB_CONNECTION // Adicionar aqui a URI de conexão do MongoDB
-  JWT_SECRET: process.env.JWT_SECRET // Adicionar aqui o JWT secret
-};
